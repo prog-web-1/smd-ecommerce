143e480e60b1c2b4bfa5881bea81e15f4cf4661d
noobmobile <luizedugx@alu.ufc.br>
2023-10-12 14:26:31 -0300

    feat: validate products amount and decrease them when a sale is done

--- back/src/venda/venda.module.ts
+++ back/src/venda/venda.module.ts
@@ -3,9 +3,10 @@ import { VendaService } from './venda.service';
 import { VendaController } from './venda.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Venda } from './entities/venda.entity';
+import { ProductModule } from '../product/product.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Venda])],
+  imports: [TypeOrmModule.forFeature([Venda]), ProductModule],
   exports: [VendaService],
   controllers: [VendaController],
   providers: [VendaService],
--- back/src/venda/venda.service.ts
+++ back/src/venda/venda.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { BaseService } from '../base/base.service';
 import { Venda } from './entities/venda.entity';
 import { CreateVendaDto } from './dto/create-venda.dto';
 import { UpdateVendaDto } from './dto/update-venda.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ProductService } from '../product/product.service';
+import { BaseError } from '../base/base.error';
 
 @Injectable()
 export class VendaService extends BaseService<
@@ -15,12 +17,42 @@ export class VendaService extends BaseService<
   constructor(
     @InjectRepository(Venda)
     private vendaRepository: Repository<Venda>,
+    private readonly productService: ProductService,
   ) {
     super(vendaRepository);
   }
 
-  create(createDto: CreateVendaDto) {
-    console.log(createDto);
-    return super.create(createDto);
+  async create(createDto: CreateVendaDto) {
+    await this.validateCartAmounts(createDto);
+    const data = await super.create(createDto);
+    await this.decreaseProductAmounts(createDto);
+    return this.findOne(data.id);
+  }
+
+  async validateCartAmounts(cart: CreateVendaDto) {
+    const products = cart.produtos;
+    const insufficient = [];
+    for (const product of products) {
+      const productInDb = await this.productService.findOne(product.produto.id);
+      if (productInDb.quantidade - product.quantidade < 0) {
+        insufficient.push(productInDb.nome);
+      }
+    }
+    if (insufficient.length > 0) {
+      const error = new BaseError();
+      error.statusCode = HttpStatus.BAD_REQUEST;
+      error.error = 'Insufficient amount';
+      error.message = insufficient;
+      throw new HttpException(error, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async decreaseProductAmounts(cart: CreateVendaDto) {
+    const products = cart.produtos;
+    for (const product of products) {
+      const productInDb = await this.productService.findOne(product.produto.id);
+      productInDb.quantidade -= product.quantidade;
+      await this.productService.update(productInDb.id, productInDb);
+    }
   }
 }
