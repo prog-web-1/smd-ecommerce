d0785512ff1359169cd549e35293708b70c8ff29
noobmobile <luizedugx@alu.ufc.br>
2023-11-17 14:40:52 -0300

    frontend

--- front/src/tools/logout.ts
+++ front/src/tools/logout.ts
@@ -1,6 +1,7 @@
 export function logout() {
-    localStorage.removeItem("user");
-    localStorage.removeItem("token");
-    localStorage.removeItem("expire_token");
-    window.location.pathname = "home";
+  localStorage.removeItem("user");
+  localStorage.removeItem("admin");
+  localStorage.removeItem("token");
+  localStorage.removeItem("expire_token");
+  window.location.pathname = "home";
 }
