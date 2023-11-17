70eb482562f04f96f3afcbd36f0c01bf275f6d39
noobmobile <luizedugx@alu.ufc.br>
2023-11-17 16:25:26 -0300

    frotnend

--- front/src/components/Layout/ModalsProvider.tsx
+++ front/src/components/Layout/ModalsProvider.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { LoginModal } from "./LoginModal/LoginModal";
 import { RegisterModal } from "./RegisterModal/RegisterModal";
 import { ProfileModal } from "./ProfileModal/ProfileModal";
+import { DeleteAccountModal } from "./DeleteAccountModal/DeleteAccountModal";
 
 export let openLoginModal: ()=>void;
 export let openRegisterModal: ()=>void;
@@ -21,6 +22,7 @@ export function ModalsProvider() {
             {isOpenLoginModal && <LoginModal showModal={isOpenLoginModal} closeModal={()=>{setIsOpenLoginModal(false)}} openRegisterModal={()=>{setIsOpenRegisterModal(true)}} />}
             {isOpenRegisterModal && <RegisterModal showModal={isOpenRegisterModal} closeModal={()=>{setIsOpenRegisterModal(false)}} />}
             {isOpenProfileModal && <ProfileModal showModal={isOpenProfileModal} closeModal={()=>{setIsOpenProfileModal(false)}} />}
+            <DeleteAccountModal />
         </>
     )
 }
