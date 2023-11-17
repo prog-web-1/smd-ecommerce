b19c1a22b40e094fde2eb26ca5a9ecc4d39a32bd
noobmobile <luizedugx@alu.ufc.br>
2023-11-17 14:36:14 -0300

    frontend

--- front/src/pages/Home/requests.ts
+++ front/src/pages/Home/requests.ts
@@ -1,38 +1,53 @@
 import { makeConnection } from "../../tools/makeConnection";
 
 export async function homeRequest() {
-    const method = "get";
-    const suffix = "category";
-    const otherQueryStrings = {
-        offset: 0,
-        limit: 10000,
-        sort: "ASC",
-        order: "nome",
-    };
+  const method = "get";
+  const suffix = "category";
+  const otherQueryStrings = {
+    offset: 0,
+    limit: 10000,
+    sort: "ASC",
+    order: "nome",
+  };
 
-    const response = (await makeConnection({method, suffix, otherQueryStrings})).data as Record<string, unknown>;
-    
-    let categories = (response.data as Record<string, unknown>[]).map(category=>{
-        return ({
-            name: category.nome as string,
-            products: (category.products as Record<string, unknown>[]).map(product=>{
-                return ({
-                    id: product.id as number,
-                    name: product.nome as string,
-                    description: product.descricao as string,
-                    stock: product.quantidade as number,
-                    price: product.preco as number,
-                    image: product.foto as string,
-                })
-            })
-        })
-    });
+  const response = (await makeConnection({ method, suffix, otherQueryStrings }))
+    .data as Record<string, unknown>;
 
-    categories = categories.filter(category=>category.products.length > 0);
+  let categories = (response.data as Record<string, unknown>[]).map(
+    (category) => {
+      return {
+        name: category.nome as string,
+        products: (category.products as Record<string, unknown>[]).map(
+          (product) => {
+            return {
+              id: product.id as number,
+              name: product.nome as string,
+              description: product.descricao as string,
+              stock: product.quantidade as number,
+              price: product.preco as number,
+              image: product.foto as string,
+            };
+          }
+        ),
+      };
+    }
+  );
 
-    categories.sort((a, b) => b.products.length - a.products.length);
+  categories = categories.map((category) => ({
+    name: category.name,
+    products: category.products.filter((product) => product.stock > 0),
+  }));
 
-    categories = categories.slice(0, 4);
+  categories = categories.filter((category) => category.products.length > 0);
 
-    return categories;
+  categories.sort((a, b) => b.products.length - a.products.length);
+
+  categories = categories.slice(0, 4);
+
+  categories = categories.map((category) => ({
+    name: category.name,
+    products: category.products.sort((a, b) => b.stock - a.stock),
+  }));
+
+  return categories;
 }
